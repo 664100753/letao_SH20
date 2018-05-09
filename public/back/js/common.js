1cfd338f4bdc57f997f62ae6af3f5b1f0a344529
664100753 <664100753@qq.com>
2018-05-09 22:44:14 +0800

    公共进度条完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,17 @@
 /**
  * Created by 张帅 on 2018/5/9.
  */
+//进度条功能的实现
+
+//禁用进度环
+NProgress.configure({ showSpinner: false });
+
+$(function(){
+  $(document).ajaxStart(function(){
+    NProgress.start();//开启进度条
+  });
+  
+  $(document).ajaxStop(function(){
+    NProgress.done();//关闭进度条
+  })
+});
