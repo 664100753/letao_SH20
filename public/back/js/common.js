3a3a75c53bffbf66e15c0838f42f465304342c12
664100753 <664100753@qq.com>
2018-05-10 11:09:23 +0800

    点击模态框完成显示

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -15,3 +15,22 @@ $(function(){
     NProgress.done();//关闭进度条
   })
 });
+
+//侧边栏动画效果
+$(function(){
+  $('.lt_topbar .icon_menu').click(function(){
+    $('.lt_aside').addClass('hidemenu');
+    $('.lt_main').addClass('hidemenu');
+    $('.lt_main .lt_topbar').addClass('hidemenu');
+  })
+});
+
+//模态框显示
+
+$(function(){
+  $('.icon_logout').on('click',function(){
+    $('#logoutModal').modal('show');
+  })
+});
+
+//点击模态框退出按钮,退出当前页面
