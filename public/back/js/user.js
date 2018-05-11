543b2bf9b4c41831c7bd3b99945f86af664d13ee
664100753 <664100753@qq.com>
2018-05-11 20:43:13 +0800

    用户页面渲染和模态框功能完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -46,4 +46,45 @@ $(function(){
    
    //已进入页面就渲染一次
   render();
+   
+   
+   //点击禁用,启用使用模态框,设置模态框
+  $('.lt_content tbody').on('click','.btn',function(){
+    $('#userModal').modal('show');
+    
+    //当点击按钮的时候,获取id发送ajax请求
+    //获取当前用户id
+    var id = $(this).parent().data('id');
+    //判断当前用户的状态,是禁用还是启用按钮
+    //判断用户需要设置成什么状态
+    var isDelete = $(this).hasClass('btn-success') ? 1 : 0;
+    
+    //点击确认按钮(模态框)
+    // 添加点击事件, 让某个用户启用禁用, 说白了, 就是重复注册了
+    // 之前注册的还存在, 导致代码重复执行了
+    // 通过 off() 可以将之前重复注册的事件移除, 再进行事件绑定, 可以保证只有一个事件绑定了
+    $('#userBtn').off().click(function(){
+      //发送ajax请求
+      $.ajax({
+        type:'post',
+        url:'/user/updateUser',
+        data:{
+          id:id,
+          isDelete:isDelete
+        },
+        dataType:'json',
+        success:function( info ){
+          //判断返回的状态
+          if(info.success){
+            //隐藏模态框
+            $('#userModal').modal('hide');
+            //重新渲染页面
+            render();
+          }
+        }
+      })
+    })
+   
+   
+  })
 });
