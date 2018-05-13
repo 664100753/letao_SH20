e08d2906692b9fa10734e45ed63e22c067ab68e9
664100753 <664100753@qq.com>
2018-05-13 12:22:42 +0800

    移动首页布局完成

--- public/front/js/common.js
+++ public/front/js/common.js
@@ -1,3 +1,25 @@
 /**
  * Created by 张帅 on 2018/5/12.
  */
+//区域滚动
+$(function () {
+  
+  //区域滚动
+  mui('.mui-scroll-wrapper').scroll({
+    indicators:false//是否显示滚动条
+  });
+  
+  
+  //图片轮播
+// /获得slider插件对象
+  var gallery = mui('.mui-slider');
+  gallery.slider({
+    interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+  });
+  
+  
+  
+  
+  
+});
+
