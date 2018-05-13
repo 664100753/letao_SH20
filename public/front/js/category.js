c4dd60dfe2763179a3c04e151f7eb222308aa9bd
664100753 <664100753@qq.com>
2018-05-13 23:16:51 +0800

    左侧分类页面渲染完成

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -1,3 +1,19 @@
 /**
  * Created by 张帅 on 2018/5/12.
  */
+//动态生成侧边栏
+$(function(){
+  
+  //发送ajax请求
+  $.ajax({
+    url:'/category/queryTopCategory',
+    type:'get',
+    dataType:'json',
+    success:function( info ){
+      console.log(info);
+      var str = template('temp',info);
+      
+      $('#cates').html(str);
+    }
+  })
+});
