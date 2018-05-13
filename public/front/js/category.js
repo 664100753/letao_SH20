706de002d8c2869bcb0f9ab247023c8b7addb458
664100753 <664100753@qq.com>
2018-05-14 00:01:24 +0800

    二级分类渲染完成

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -10,10 +10,46 @@ $(function(){
     type:'get',
     dataType:'json',
     success:function( info ){
-      console.log(info);
+      // console.log(info);
       var str = template('temp',info);
       
       $('#cates').html(str);
+      //获取第一条信息
+      renderId(info.rows[0].id);
     }
-  })
+  });
+  
+  
+  //二级分类页面渲染
+  //注册事件委托
+  $('#cates').on('click','a',function(){
+    //获取id
+    var id = $(this).data('id');
+    // console.log(id);
+    //排他
+    $(this).addClass('current').parent().siblings().children().removeClass('current');
+    
+    //发送ajax请求
+    renderId(id);
+    
+  });
+  
+  function renderId(id) {
+    $.ajax({
+      type:'get',
+      url:'/category/querySecondCategory',
+      data:{
+        id:id
+      },
+      dataType:'json',
+      success:function(info){
+        console.log(info);
+        var str = template('tempRight',info);
+      
+        $('#ul_right').html(str);
+      }
+    })
+    
+  }
+ 
 });
